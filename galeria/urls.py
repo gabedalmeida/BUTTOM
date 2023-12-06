70d969bf911813e582c1a36eb9701177ec3d92f4
gabedalmeida <gabrelalmeida17@icloud.com>
2023-12-06 09:30:05 +0100

    Iniciando projeto

--- galeria/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from galeria.views import test, imagem
-
-urlpatterns = [
-    path('', test, name='test'),
-    path('imagem/<int:foto_id>', imagem, name='imagem'),
-]
