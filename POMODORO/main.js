a53f0848243119b603b1568efb0fbb46d7bf43e8
gabedalmeida <gabrelalmeida17@icloud.com>
2024-01-11 11:24:33 +0100

    comunicando o fim datarefa e alterando o card

--- POMODORO/main.js
+++ POMODORO/main.js
@@ -83,6 +83,11 @@ const contagemRegressiva = () => {
     if(tempoDecorridoSegundos <= 0){
         audioTempoFinalizado.play()
         alert('Tempo finalizado!');
+        const focoAtivo = html.getAttribute('data-contexto') == 'foco';
+        if(focoAtivo){
+            const evento = new CustomEvent('FocoFinalizado');
+            document.dispatchEvent(evento)
+        }
         zerar();
         return ;
     }
--- POMODORO/script-crud.js
+++ POMODORO/script-crud.js
@@ -3,6 +3,7 @@ const formAdicionarTarefa = document.querySelector('.app__form-add-task');
 const textArea = document.querySelector('.app__form-textarea');
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
 let tarefaSelecionada = null;
+let liTarefaSelecionada = null;
 
 function atualizarTarefas () {
     localStorage.setItem('tarefas', JSON.stringify(tarefas));
@@ -61,10 +62,12 @@ function criarElementoTarefa(tarefa) {
         if(tarefaSelecionada == tarefa){
             paragrafoDescricaoTarefa.textContent = " ";
             tarefaSelecionada = null;
+            liTarefaSelecionada = null;
             return
 
         }
         tarefaSelecionada = tarefa
+        liTarefaSelecionada = li
         paragrafoDescricaoTarefa.textContent = tarefa.descricao;
        
         li.classList.add('app__section-task-list-item-active');
@@ -96,3 +99,13 @@ tarefas.forEach(tarefa => {
    ulTarefa.append(elementoTarefa);
 
 });
+
+document.addEventListener('FocoFinalizado', () => {
+    if(tarefaSelecionada && liTarefaSelecionada) {
+        liTarefaSelecionada.classList.remove('app__section-task-list-item-active');
+        liTarefaSelecionada.classList.add('app__section-task-list-item-complete');
+        liTarefaSelecionada.querySelector('button').setAttribute('disabled', 'disabled')
+        
+    }
+
+})
